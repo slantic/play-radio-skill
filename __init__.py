65f9999e39d9035e8b784756536afeb4ff2e495c
Slantic <pascalvannugteren@gmail.com>
2022-09-07 22:08:20 +0200

    Basic version, npo 1 and npo 2

--- __init__.py
+++ __init__.py
@@ -1,14 +1,38 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
+
+from mycroft.util.parse import match_one
 
 
-class PlayRadio(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+track_dict = {
+    'npo radio 1': 'http://icecast.omroep.nl/radio1-bb-mp3',
+    'npo radio 2': 'http://icecast.omroep.nl/radio2-bb-mp3'
+}
 
-    @intent_file_handler('radio.play.intent')
-    def handle_radio_play(self, message):
-        self.speak_dialog('radio.play')
+class PlayRadio(CommonPlaySkill):
+    def CPS_match_query_phrase(self, phrase):
+        """ This method responds wether the skill can play the input phrase.
+            The method is invoked by the PlayBackControlSkill.
+            Returns: tuple (matched phrase(str),
+                            match level(CPSMatchLevel),
+                            optional data(dict))
+                     or None if no match was found.
+        """
+        # Get match and confidence
+        match, confidence = match_one(phrase, track_dict)
+        # If the confidence is high enough return a match
+        if confidence > 0.5:
+            return (match, CPSMatchLevel.EXACT, {"track": match})
+        # Otherwise return None
+        else:
+            return None
 
+    def CPS_start(self, phrase, data):
+        """ Starts playback.
+            Called by the playback control skill to start playback if the
+            skill is selected (has the best match level)
+        """
+        url = data['track']
+        self.audioservice.play(url)
 
 def create_skill():
     return PlayRadio()
